2ff7298eeeda347ac1c47b8673efa672a73033b1
Allyssa <118028111+Allyssacodes@users.noreply.github.com>
2023-07-25 21:53:04 -0400

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,49 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 720;
+	
+	
+class InputHandler {
+	constructor() {
+		this.keys = [];
+		window.addEventListener('keydown', function(e){
+			console.log(e.key);
+		})
+	}
+}
+	
+class Player {
+	constructor() {
+			
+	}
+}
+	
+	
+class Backgrounds {
+	constructor() {
+			
+    }
+}
+	
+	
+class Enemy {
+	constructor() {
+			
+	}
+}
+	
+function handleEnemies() {
+		
+}
+	
+function displayStatusText() {
+		
+}
+	
+let input = new InputHandler();
+console.log(2 + 1)
+	
+function animate() {
+		
+}
